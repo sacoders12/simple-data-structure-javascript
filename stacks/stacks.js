b56ae512bf9923826bcbc8e5faba7bdbd2b815ae
agung <hermawan.agung12@gmail.com>
2021-05-26 12:43:09 +0700

    stack

---
+++ stacks/stacks.js
@@ -0,0 +1,37 @@
+var createStack  = function(){
+    this.count = 0;
+    this.storage = {};
+
+    this.push = function(value){
+        this.storage[this.count] = value;
+        this.count++;
+    }
+
+    this.pop = function(){
+        if(this.count === 0){
+            return undefined;
+        }
+
+        this.count--;
+        var result = this.storage[this.count];
+        delete this.storage[this.count];
+        return result;
+    }
+
+    this.size = function(){
+        return this.count;
+    }
+
+    this.peek = function(){
+        return this.storage[this.count-1];
+    }
+
+}
+
+var myStack = new createStack();
+
+myStack.push(1);
+myStack.push(2);
+console.log(myStack.peek());
+console.log(myStack.pop());
+console.log(myStack.pop());
